3e138e5fc778714458daafa481121811cf9dbcfc
Danylo Shlapak <geografy2314@gmail.com>
2025-03-30 18:10:34 +0300

    Add input, output functions

---
+++ app/io/output.py
@@ -0,0 +1,20 @@
+def print_to_console(text):
+    """
+    Print text to the console.
+
+    Args:
+        text (str): Text for printing.
+    """
+    print(text)
+
+
+def write_to_file(text, file_path):
+    """
+    Write text to the specified file.
+
+    Args:
+        text (str): Text to be written.
+        file_path (str): Path to the file.
+    """
+    with open(file_path, "w") as file:
+        file.write(text)
